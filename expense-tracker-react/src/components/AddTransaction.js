a01fedb1ca0ec12c8ba24843df44638a21ed5869
Leon Gabriel <g73346920@gmail.com>
2024-09-04 00:23:21 +0300

    update the use of Hook

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import React from 'react'
 
  const AddTransaction = () => {
+  const [text,setText]= useState('')
+const [amount,setAmount]=useState('')
+
   return (
     <>
 <h3>Add New transaction</h3>
 <form>
   <div className='form-control'>
-    <label for="text">Text</label>
-    <input type='text' placeholder='Enter text...' />
+    <label htmlfor="text">Text</label>
+    <input type='text' value={text} onChange={(e)=>setText(e.target.value)} placeholder='Enter text...' />
   </div>
+  <div className='form-control'>
+    <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
+    <input type='number' value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder='Enter amount ...' />
+  </div>
+  <button className='btn'>Add transaction</button>
 </form>
     </>
   )
