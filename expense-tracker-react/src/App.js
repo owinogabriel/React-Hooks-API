0e5ab9eb89a7680aef014c29b1bb0ef25e5c92df
Leon Gabriel <g73346920@gmail.com>
2024-09-03 23:49:22 +0300

    import all the files exported

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Balance from './components/Balance';
+import  Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
     <div>
-  
+   <Header />
+   <div className='container'>
+  <Balance />
+  <IncomeExpenses />
+   </div>
     </div>
   );
 }
