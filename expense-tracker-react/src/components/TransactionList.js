b94ed0de0a4d0ca5222e0bff318d598445a389a7
Leon Gabriel <g73346920@gmail.com>
2024-09-04 12:41:01 +0300

    new prop asigned

--- expense-tracker-react/src/components/TransactionList.js
+++ expense-tracker-react/src/components/TransactionList.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
 
 const TransactionList = () => {
+  const { transaction } = useContext(GlobalContext);
+
   return (
-   <>
-   <h3>History</h3>
-   <ul id='list' className='list'>
-    <li className='minus'>
-cash <span>-$400</span><button className='delete-btn'>x</button>
-    </li>
-   </ul>
-   </>
-  )
+    <>
+      <h3>History</h3>
+      <ul  className='list'>
+        {transaction.map((trans) => (
+          <li key={trans.id} className={trans.amount < 0 ? 'minus' : 'plus'}>
+            {trans.text} <span>{trans.amount < 0 ? '-' : '+'}${Math.abs(trans.amount)}</span>
+            <button className='delete-btn'>x</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default TransactionList;
