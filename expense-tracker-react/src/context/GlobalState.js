0bb29a19846829eab1e961e12424a419a00d6baa
Leon Gabriel <g73346920@gmail.com>
2024-09-04 12:43:58 +0300

    initial state included

--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -4,9 +4,9 @@ import AppReducer from './AppReducer'
 const initialState={
 transaction:[
 {id:1, text:'Flower', amount: -20 },
-{id:2, text:'Salary', amount: -300 },
+{id:2, text:'Salary', amount: +300 },
 {id:3, text:'Book', amount: -10 },
-{id:4, text:'Camera', amount: -150 }
+{id:4, text:'Camera', amount: +150 }
 
 ]
 };
