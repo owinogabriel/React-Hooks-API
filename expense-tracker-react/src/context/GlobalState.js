e0c5e2d5a95a385fbe35ef091e0e541ecd62d860
Leon Gabriel <g73346920@gmail.com>
2024-09-04 15:29:08 +0300

    Last commit

--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -1,26 +1,43 @@
-import  React, {createContext, useReducer} from 'react';
-import AppReducer from './AppReducer'
-// initial state
-const initialState={
-transaction:[
-{id:1, text:'Flower', amount: -20 },
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// Initial state
+const initialState = {
+  transactions: [
+    {id:1, text:'Flower', amount: -20 },
 {id:2, text:'Salary', amount: +300 },
 {id:3, text:'Book', amount: -10 },
 {id:4, text:'Camera', amount: +150 }
+  ]
+}
 
-]
-};
-
-//context
+// Create context
 export const GlobalContext = createContext(initialState);
 
-//provider
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-export const GlobalProvider =({children})=>{
-  const  [state,dispatch] = useReducer(AppReducer,initialState);
-  return(<GlobalContext.Provider value={{
-    transaction:state.transaction
-  }}>'
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+  }}>
     {children}
-  </GlobalContext.Provider>)
+  </GlobalContext.Provider>);
 }
