0c6042f2760a3e20a680df844b8f22f2e304e47c
Leon Gabriel <g73346920@gmail.com>
2024-09-05 00:32:24 +0300

    Import Home.js added

--- commerce/src/App.js
+++ commerce/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Home from './component/Home';
 import Header from './component/Navbar';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
