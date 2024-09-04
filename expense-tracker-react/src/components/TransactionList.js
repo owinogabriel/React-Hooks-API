d9922e744fc83f2c7c530abeb3b0feef6b9a862b
Leon Gabriel <g73346920@gmail.com>
2024-09-04 15:28:14 +0300

    Update:imported file

--- expense-tracker-react/src/components/TransactionList.js
+++ expense-tracker-react/src/components/TransactionList.js
@@ -1,22 +1,17 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
 import { GlobalContext } from '../context/GlobalState';
 
-
-const TransactionList = () => {
-  const { transaction } = useContext(GlobalContext);
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
 
   return (
     <>
       <h3>History</h3>
-      <ul  className='list'>
-        {transaction.map((trans) => (
-          <li key={trans.id} className={trans.amount < 0 ? 'minus' : 'plus'}>
-            {trans.text} <span>{trans.amount < 0 ? '-' : '+'}${Math.abs(trans.amount)}</span>
-            <button className='delete-btn'>x</button>
-          </li>
-        ))}
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </>
-  );
-};
-export default TransactionList;
+  )
+}
