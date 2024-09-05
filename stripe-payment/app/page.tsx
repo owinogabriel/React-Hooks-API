bbb53b660c6356576b59ce77ea762ca4f2e1b3f9
Leon Gabriel <g73346920@gmail.com>
2024-09-05 21:32:30 +0300

    LoadStripe and Elements imported

--- stripe-payment/app/page.tsx
+++ stripe-payment/app/page.tsx
@@ -1,4 +1,14 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { error } from "console";
 
+// import CheckoutForm from "../components/CheckoutForm";()
+// import CompletePage from "../components/CompletePage";
+
+if(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined){
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined")
+}
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Home() {
   return (
