63a184ed64a8e667c8fdebfb2e02457356b41bed
Leon Gabriel <g73346920@gmail.com>
2024-09-06 00:21:23 +0300

    h2:Modified

--- stripe-payment/app/page.tsx
+++ stripe-payment/app/page.tsx
@@ -7,6 +7,7 @@ import convertTosubcurrency from "@/lib/convertToSubcurrency"
 // import CompletePage from "../components/CompletePage";
 import CheckoutPage from "@/components/CheckoutPage";
 
+
 if(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined){
   throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined")
 }
@@ -17,8 +18,8 @@ export default function Home() {
   return (
   <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
     <div className="mb-10">
-      <div className="text-4xl font-extrabold mb-2">Sonny
-      <h2 className="text-2xl">
+      <div className="text-4xl font-extrabold mb-2">Gabriel    
+        <h2 className="text-2xl">
         has requested <span className="font-bold">${amount}</span>
       </h2>
       </div>
