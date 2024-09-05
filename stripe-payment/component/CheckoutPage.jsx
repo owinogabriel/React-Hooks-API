284e4e124a3d126614a0c2b00e9224f0ef1feb80
Leon Gabriel <g73346920@gmail.com>
2024-09-05 23:17:20 +0300

    Anytime the amount changes we genarate the clientSecret payment

--- stripe-payment/component/CheckoutPage.jsx
+++ stripe-payment/component/CheckoutPage.jsx
@@ -13,5 +13,17 @@ const CheckoutPage = ({amount}, { amount: number }) =>{
   const [errorMessage, setError] =useStete <string>('');
   const [clientSecret, setClientSecret] =useStete('');
   const [loading, setlLoading] =useStete<string>(false);
+ useEffect (() =>{
+  fetch("/api/create-payment-intent",{
+    method:"POST",
+    headers:{
+      "content-Type": "application/json",
+    },
+    body:JSON.stringify({amount: convertToSubcurrency(amount)}),
+  })
+  .then((res) => res.json())
+  .then((data) => setClientSecret(data.clientSecret))
+ },[amount])
+
 };
 export default CheckoutPage;
