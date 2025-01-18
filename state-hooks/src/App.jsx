ce1b2481d34986722ccecbe615538cacb3f95f4b
Leon Gabriel <g73346920@gmail.com>
2025-01-18 16:53:39 +0300

    Normalize line endings

--- state-hooks/src/App.jsx
+++ state-hooks/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UseState from './hooks/use-state'
+import Effect from './hooks/use-effect'
+import Pedro from './hooks/use-effect/effect'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,8 @@ function App() {
   return (
     <>
      <UseState />
+     {/* <Effect /> */}
+     <Pedro />
     </>
   )
 }
